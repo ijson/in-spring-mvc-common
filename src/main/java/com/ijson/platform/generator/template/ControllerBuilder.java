0d80a354e68b88d70f7e09d3e94e27ec283ee4a1
cuiyx <414648691@qq.com>
2017-09-25 10:46:37 +0800

    update pom

---
+++ src/main/java/com/ijson/platform/generator/template/ControllerBuilder.java
@@ -0,0 +1,22 @@
+package com.ijson.platform.generator.template;
+
+import com.ijson.platform.api.model.ParamsVo;
+import com.ijson.platform.generator.model.TableEntity;
+
+import java.util.Map;
+
+/**
+ * Created by cuiyongxu on 17/9/24.
+ */
+public class ControllerBuilder implements TemplateHanlder {
+    /**
+     * 代码生成器模板接口
+     *
+     * @param vo     方法参数
+     * @param config config
+     */
+    @Override
+    public void execute(ParamsVo<TableEntity> vo, Map<String, String> config) {
+
+    }
+}
