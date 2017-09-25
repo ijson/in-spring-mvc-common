af811bb41db3ce6cc00884f7bb9da3a8024a912e
cuiyx <414648691@qq.com>
2017-09-25 15:18:35 +0800

    update hiberndate

--- src/main/java/com/ijson/platform/generator/template/HibernateXmlBuilder.java
+++ src/main/java/com/ijson/platform/generator/template/HibernateXmlBuilder.java
@@ -1,11 +1,13 @@
 package com.ijson.platform.generator.template;
 
+import com.google.common.collect.Maps;
 import com.ijson.platform.api.model.ParamsVo;
 import com.ijson.platform.common.util.FileOperate;
 import com.ijson.platform.common.util.Validator;
 import com.ijson.platform.generator.model.ColumnEntity;
 import com.ijson.platform.generator.model.TableEntity;
 import com.ijson.platform.generator.util.DataType;
+import com.ijson.platform.generator.util.TemplateUtil;
 
 import java.util.List;
 import java.util.Map;
@@ -20,49 +22,20 @@ public class HibernateXmlBuilder implements TemplateHanlder {
     }
 
     private void createdHibernateXml(String prefix, List<TableEntity> tables, Map<String, String> config) {
-        String jarPath = config.get("package_name");
         String projectName = config.get("project_name");
         String xmlPath = config.get("fs_path") + "/" + projectName + "/" + prefix + "resources/hibernate/";
         FileOperate.getInstance().newCreateFolder(xmlPath);
         if (!Validator.isEmpty(tables)) {
             for (TableEntity table : tables) {
-                StringBuilder result = new StringBuilder("");
-                result.append("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
-                result.append("<!DOCTYPE hibernate-mapping PUBLIC \"-//Hibernate/Hibernate Mapping DTD 3.0//EN\" \n");
-                result.append("\"http://hibernate.sourceforge.net/hibernate-mapping-3.0.dtd\">\n\n");
-                result.append("<hibernate-mapping>\n");
-                result.append("    <class name=\"").append(jarPath).append(".entity.").append(table.getTableAttName()).append("\" table=\"").append(table.getTableName()).append("\" >\n");
-
-                result.append(getResultMap(table));
-
-                result.append("    </class>\n");
-                result.append("</hibernate-mapping>\n");
-                FileOperate.getInstance().newCreateFile(xmlPath + table.getTableAttName() + ".hbm.xml",
-                        result.toString());
+                String entityName = table.getTableAttName();
+                String tableName = table.getTableName();
+                Map<String,Object> maps = Maps.newHashMap();
+                maps.put("entityName", entityName);
+                maps.put("tableName", tableName);
+                maps.put("table",table);
+                FileOperate.getInstance().newCreateFile(xmlPath + table.getTableAttName() + ".hbm.xml", TemplateUtil.getTemplate("hibernate.ijson", maps));
             }
         }
     }
 
-    private String getResultMap(TableEntity table) {
-        StringBuilder result = new StringBuilder("");
-        int count = table.getColumns().size();
-        if (count > 0) {
-            for (int i = 0; i < count; i++) {
-                ColumnEntity col = table.getColumns().get(i);
-                String colType = DataType.getDataType(col.getColumnTypeName(), true, col.getPrecision());
-
-                if (col.getAttrName().equalsIgnoreCase(table.getPKColumn())) {
-                    result.append("    <id name=\"").append(col.getAttrName()).append("\" type=\"").append(colType).append("\">\n");
-                    result.append("        <column name=\"").append(col.getColumnName()).append("\" length=\"").append(col.getPrecision()).append("\" />\n");
-                    result.append("        <generator class=\"assigned\" />\n");
-                    result.append("    </id>\n");
-                } else {
-                    result.append("    <property name=\"").append(col.getAttrName()).append("\" type=\"").append(colType).append("\">\n");
-                    result.append("        <column name=\"").append(col.getColumnName()).append("\" length=\"").append(col.getPrecision()).append("\" />\n");
-                    result.append("    </property>\n");
-                }
-            }
-        }
-        return result.toString();
-    }
 }
